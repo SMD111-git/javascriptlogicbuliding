5789b41f6f332b610dc2a3ce3d526a17e3b9eed3
SMD111-git <owaissmd99@gmail.com>
2025-07-02 14:03:18 +0530

    counting approch two

--- countingnumber.js
+++ countingnumber.js
@@ -6,3 +6,16 @@ const n= 12345
 console.log("N:"+n)
 const digitcount=count(n)
 console.log("number of digits:",digitcount)
+///approcha two
+/**function count(n){
+    let cnt=0
+    while(n!==0){
+        n=Math.floor(n/10)
+        cnt++
+    }
+    return cnt++
+
+}
+let n= 123
+console.log(count(n))
+ */
